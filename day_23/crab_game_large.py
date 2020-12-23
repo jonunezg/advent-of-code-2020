da8904efc8825da82dbcdb009d20ae419262f292
Jose Luis Nunez <8852923+jonunezg@users.noreply.github.com>
2020-12-23 12:10:49 -0800

    Slight change

--- day_23/crab_game_large.py
+++ day_23/crab_game_large.py
@@ -1,27 +1,25 @@
 from os import path
 
-def play(s, cur):
-    next_3 = [s[cur], s[s[cur]], s[s[s[cur]]]]
-    s[cur] = s[next_3[2]]
-    i = cur - 1
-    while True:
-        if i == 0:
-            i = len(s) - 1
-        if i not in next_3:
-            break
-        i -= 1
-    s[i], s[next_3[2]] = next_3[0], s[i]
-    return s[cur]
+def next(candidate, exclusions, max_value):
+    while candidate in exclusions:
+        candidate = candidate - 1 if candidate > 1 else max_value
+    return candidate
 
-with open(path.dirname(__file__) + '/input2.txt') as f:
+def play(s, cur):
+    next_3 = [s[cur], s[s[cur]], s[s[s[cur]]]] # Get 3 elements to move
+    s[cur] = s[next_3[2]] # Move them out of the list
+    i = next(cur, next_3 + [cur], len(s) - 1) # Find the destination
+    s[i], s[next_3[2]] = next_3[0], s[i] # Move 3 elements to destination
+    return s[cur] # Return right neighbor of current value
+
+with open(path.dirname(__file__) + '/input.txt') as f:
     print('Play the game and print the product of the two numbers to the right of 1')
-    sequence = [i + 1 for i in range(1000001)]
+    sequence = [max(10, i + 1) for i in range(1000001)]
     input = f.read().strip('\n')
     for i, c in enumerate(input[:-1]):
         sequence[int(c)] = int(input[i + 1])
-    sequence[int(input[-1])] = 10
     sequence[-1] = int(input[0])
     current = int(input[0])
     for i in range(10000000):
         current = play(sequence, current)
-    print(sequence[1], sequence[sequence[1]])
+    print(sequence[1] * sequence[sequence[1]])
