8f4d6e9a41e35458146431952d7ef03ccefbc54e
Jose Luis Nunez <8852923+jonunezg@users.noreply.github.com>
2020-12-25 08:29:27 -0800

    Shorter

--- src/day_25/encryption_key.py
+++ src/day_25/encryption_key.py
@@ -3,17 +3,9 @@ from os import path
 def update(n, subject):
     return (n * subject) % 20201227
 
-def apply(subject, i):
-    n = 1
-    for _ in range(i):
-        n = update(n, subject)
-    return n
-
 with open(path.dirname(__file__) + '/input.txt') as f:
     print('Get the encryption_key')
-    public_keys = [int(line.strip('\n')) for line in f.readlines()]
-    n, i = 1, 0
+    n, encryption_key, public_keys = 1, 1, [int(line.strip('\n')) for line in f.readlines()]
     while n != public_keys[0]:
-        n = update(n, 7)
-        i += 1
-    print(apply(public_keys[1], i))
+        n, encryption_key = update(n, 7), update(encryption_key, public_keys[1])
+    print(encryption_key)
